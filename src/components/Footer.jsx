adfee73e55cfbec7ef8015b0ad0366c9a5867d35
neyryvargasd <neyry007@gmail.com>
2023-08-04 20:37:09 -0500

    consumo Api

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Footer.css'
+
+
+export default function Footer() {
+  return (
+    <div className='Footer'>
+      <div className='footer-up'>
+        <div className='footer-p'>
+        <p></p>
+        </div>
+
+        <div className="footer-c">
+                    <p>Crypto Stadistics</p>
+                    <p>© 2023</p>
+        </div>
+
+        <div className='footer-down'>
+          <p>Developed by: Neyry VD</p>
+
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
